200d2c68535a86570fb58138ee81670284630872
awesh <aweshprajapati150@gmail.com>
2024-10-17 01:33:14 +0530

    17-10-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_crud_app/provider/counter_provider.dart';
-import 'package:provider_crud_app/provider/home_provider.dart';
+
 import 'package:provider_crud_app/view/counter_page.dart';
-import 'package:provider_crud_app/view/home_page.dart';
+
 
 void main()
 {
@@ -18,14 +18,13 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => CounterProvider(),),
-        ChangeNotifierProvider(create: (context) => BackgroundProvider(),),
+
       ],
 
       builder: (context, child) =>MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
           '/':(context)=>CounterPage(),
-          '/home':(context)=>HomePage(),
         },
       ),
     );
