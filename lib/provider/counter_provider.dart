6c2811824c6cb35ba644930ea3641369ce6833c5
Avesh6754 <aweshprajapati150@gmail.com>
2024-10-20 10:58:14 +0530

    20-10-2024

--- lib/provider/counter_provider.dart
+++ lib/provider/counter_provider.dart
@@ -1,22 +1,55 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:provider_crud_app/utils/global.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterProvider extends ChangeNotifier{
   int counter=0;
   int round=0;
   int totalcount=0;
-
+  int index=0;
+  void background_Right_to_left()
+  {
+    if(index!=backgroundlist.length-1)
+    {
+      index++;
+    }
+    else
+    {
+      index=0;
+    }
+    saveappdata(counter, totalcount, round, index);
+    notifyListeners();
+  }
+  void background_Left_to_Right()
+  {
+    if(index==0)
+    {
+      index=backgroundlist.length-1;
+    }
+    else
+    {
+      index--;
+    }
+    saveappdata(counter, totalcount, round, index);
+    notifyListeners();
+  }
   void incrementcounter()
   {
     counter++;
+
    if(counter>108)
      {
        counter=1;
      }
+    saveappdata(counter, totalcount, round, index);
     notifyListeners();
   }
   void totalcounting()
   {
     totalcount++;
+    saveappdata(counter, totalcount, round, index);
     notifyListeners();
   }
   void decrementcounter()
@@ -26,12 +59,14 @@ class CounterProvider extends ChangeNotifier{
         totalcount--;
         counter--;
       }
+    saveappdata(counter, totalcount, round, index);
     notifyListeners();
   }
   void restartcounter()
   {
 
       counter=0;
+      saveappdata(counter, totalcount, round, index);
     notifyListeners();
   }
   void counterround()
@@ -40,6 +75,36 @@ class CounterProvider extends ChangeNotifier{
       {
         round++;
       }
+    saveappdata(counter, totalcount, round, index);
     notifyListeners();
+  }
+
+
+  Future<void> saveappdata(int count,int total,int totalround,int value)
+  async {
+    SharedPreferences sharedPreferences= await SharedPreferences.getInstance();
+   bool status= await sharedPreferences.setInt('count', count);
+     await sharedPreferences.setInt('total', total);
+    await sharedPreferences.setInt('round', totalround);
+    await sharedPreferences.setInt('index', value);
+    if(status)
+      {
+        log('Data Save :$count');
+      }
+  }
+  Future<void> getData()
+  async {
+    SharedPreferences sharedPreferences=await SharedPreferences.getInstance();
+   counter= sharedPreferences.getInt('count')??0;
+   //??null replacement operator if the value null to assign 0;
+   totalcount= sharedPreferences.getInt('total')??0;
+   round= sharedPreferences.getInt('round')??0;
+   index= sharedPreferences.getInt('index')??0;
+   notifyListeners();
+  }
+
+  CounterProvider()
+  {
+    getData();
   }
 }
--- lib/view/counter_page.dart
+++ lib/view/counter_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_crud_app/provider/counter_provider.dart';
-
+import 'package:provider_crud_app/utils/global.dart';
 
 import 'component/counter_container.dart';
 import 'component/counter_details.dart';
@@ -19,21 +19,58 @@ class CounterPage extends StatelessWidget {
         Provider.of<CounterProvider>(context, listen: false);
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(backgroundlist[counterProvidertrue.index]),
+                fit: BoxFit.cover)),
         alignment: Alignment.center,
         child: Stack(
           children: [
             CounterDetails(counterProvidertrue: counterProvidertrue),
-            Counter_Container(counterProviderfalse: counterProviderfalse, counterProvidertrue: counterProvidertrue),
+            Counter_Container(
+                counterProviderfalse: counterProviderfalse,
+                counterProvidertrue: counterProvidertrue),
+            Align(
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                            Colors.black.withOpacity(0.58))),
+                    alignment: Alignment.center,
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_outlined,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      counterProviderfalse.background_Right_to_left();
+                    },
+                  ),
+                  IconButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                            Colors.black.withOpacity(0.58))),
+                    alignment: Alignment.center,
+                    icon: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      counterProviderfalse.background_Left_to_Right();
+                    },
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingAction(counterProviderfalse: counterProviderfalse,),
+      floatingActionButton: FloatingAction(
+        counterProviderfalse: counterProviderfalse,
+      ),
     );
   }
 }
-
-
-
-
-
-
