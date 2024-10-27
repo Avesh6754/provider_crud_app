55fb881479d37e3f85cf741f178237b48d0e4d6c
Avesh6754 <aweshprajapati150@gmail.com>
2024-10-27 20:07:50 +0530

    27-10-2024

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_crud_app/gallery_authentication/provider/galleryhome_provider.dart';
 import 'package:provider_crud_app/todo_app/provider/TodoProvider.dart';
+import 'package:provider_crud_app/todo_app/provider/themeProvider.dart';
 import 'package:provider_crud_app/todo_app/view/todo_home%20_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,11 +12,8 @@ import 'chanting_app/provider/counter_provider.dart';
 import 'gallery_authentication/view/home/gallery_homePage.dart';
 
 bool currenttheme=false;
-Future<void> main()
-async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences sharedPreferences=await SharedPreferences.getInstance();
-  currenttheme=sharedPreferences.getBool('theme')??false;
+void main()
+ {
 
   runApp(MyApp());
 }
@@ -29,14 +27,15 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => CounterProvider(),),
         ChangeNotifierProvider(create: (context) => GalleryHome_Provider(),),
-        ChangeNotifierProvider(create: (context) => TodoProvider(currenttheme),),
+        ChangeNotifierProvider(create: (context) => TodoProvider(),),
+        ChangeNotifierProvider(create: (context) => ThemeProvider(),),
       ],
 
       builder: (context, child) =>MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData.light(),
         darkTheme: ThemeData.dark(),
-        themeMode:Provider.of<TodoProvider>(context).isDark?ThemeMode.dark:ThemeMode.light,
+        themeMode:Provider.of<ThemeProvider>(context).isDark?ThemeMode.dark:ThemeMode.light,
         routes: {
           '/':(context)=>Todo_App(),
         },
--- lib/todo_app/view/todo_home _page.dart
+++ lib/todo_app/view/todo_home _page.dart
@@ -5,84 +5,228 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_crud_app/todo_app/provider/TodoProvider.dart';
 import 'package:provider_crud_app/todo_app/utils/global.dart';
-import 'package:provider_crud_app/todo_app/view/component/datetime.dart';
 
-import 'component/alertdialodbox.adrt.dart';
-import 'component/completedtask.dart';
-import 'component/pendingtask.dart';
-import 'component/profile.dart';
+import '../provider/themeProvider.dart';
+
 
 class Todo_App extends StatelessWidget {
   const Todo_App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> formkey = GlobalKey<FormState>();
+    TodoProvider providertrue =
+    Provider.of<TodoProvider>(context, listen: true);
+    TodoProvider providerfalse =
+    Provider.of<TodoProvider>(context, listen: false);
+
+    ThemeProvider themeProvider=Provider.of<ThemeProvider>(context,listen: true);
+    ThemeProvider themeProviderfales=Provider.of<ThemeProvider>(context,listen: false);
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'TODO App',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          Switch(value:themeProvider.isDark , onChanged: (value) {
+            themeProviderfales.themeChange();
+          },)
+        ],
+      ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Profiledata(),
-              const SizedBox(
-                height: 20,
-              ),
-              DateTimeClass(),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.grey),
-                alignment: Alignment.center,
-                child: const Row(
-                  children: [PendingTask(), Completedtask()],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ...List.generate(
-                Provider.of<TodoProvider>(context, listen: true)
-                    .taskList
-                    .length,
-                (index) => ListTile(
-                  title: Text(
-                      '${Provider.of(context, listen: true).taskList[index]}'),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5,
+            ),
+            ...List.generate(
+              providertrue.studentdata.length,
+                  (index) => Card(
+                child: ListTile(
+                  leading:
+                  CircleAvatar(radius: 20, child: Text('${index + 1}')),
+                  title: Text('${providertrue.studentdata[index].name}'),
                   subtitle: Text(
-                      '${Provider.of(context, listen: true).descriptionList[index]}'),
-                  trailing: Text(
-                      '${Provider.of(context, listen: true).datetime[index]}'),
-                  leading: Checkbox(
-                    value: bool.parse(
-                        Provider.of(context, listen: true).status[index]),
-                    onChanged: (v) {
-                      Provider.of(context, listen: false)
-                          .updatestatus(v!, index);
-                    },
+                      '${providertrue.studentdata[index].id}\n${providertrue.studentdata[index].dateTime}'),
+                  trailing: Container(
+                    width: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              txtid.text = providertrue.studentdata[index].id;
+                              txtname.text =
+                                  providertrue.studentdata[index].name;
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Padding(
+                                    padding: const EdgeInsets.all(2),
+                                    child: Form(
+                                      key: formkey,
+                                      child: Column(
+                                        children: [
+                                          const Text(
+                                            'Update Task',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(
+                                            height: 20,
+                                          ),
+                                          TextFormField(
+                                            controller: txtname,
+                                            decoration: InputDecoration(
+                                              label: Text('Task'),
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                BorderRadius.circular(10),
+                                                borderSide: const BorderSide(
+                                                    color: Colors.blue,
+                                                    width: 2),
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 20,
+                                          ),
+                                          TextField(
+                                            controller: txtid,
+                                            maxLines: 2,
+                                            decoration: InputDecoration(
+                                              label: const Text('Description'),
+                                              border: OutlineInputBorder(
+                                                borderRadius:
+                                                BorderRadius.circular(10),
+                                                borderSide: const BorderSide(
+                                                    color: Colors.blue,
+                                                    width: 2),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          if (formkey.currentState!
+                                              .validate()) {
+                                            Navigator.of(context).pop();
+                                            providerfalse.updatestudent(
+                                                txtid!.text,
+                                                txtname!.text,
+                                                DateTime.now(),
+                                                index);
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                content: Text('Update Save'),
+                                                duration: Duration(seconds: 2),
+                                              ),
+                                            );
+                                          }
+                                        },
+                                        child: Text('Save'))
+                                  ],
+                                ),
+                              );
+                            },
+                            icon: Icon(Icons.edit)),
+                        IconButton(
+                            onPressed: () {
+                              providerfalse.removestudent(index);
+                            },
+                            icon: Icon(Icons.delete))
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
         onPressed: () {
+          txtname=TextEditingController();
+          txtid=TextEditingController();
           showDialog(
             context: context,
-            builder: (context) => AlertDialodbox(),
+            builder: (context) => AlertDialog(
+              title: Padding(
+                padding: const EdgeInsets.all(2),
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                    children: [
+                      Text('Add Task'),
+                      TextFormField(
+                        validator: (value) {
+                          return value!.isEmpty
+                              ? "This deatils is mandatory"
+                              : null;
+                        },
+                        controller: txtname,
+                        decoration: InputDecoration(
+                            label: Text('Task'),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(
+                                    color: Colors.blue, width: 2))),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          return value!.isEmpty
+                              ? "This deatils is mandatory"
+                              : null;
+                        },
+                        controller: txtid,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          label: Text('Description'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                            BorderSide(color: Colors.blue, width: 2),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        Navigator.of(context).pop();
+                        providerfalse.addstudent(
+                          txtid!.text,
+                          txtname!.text,
+                          DateTime.now(),
+                        );
+
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Task Save'),
+                          duration: Duration(seconds: 2),
+                        ));
+                      }
+                    },
+                    child: Text('Save'))
+              ],
+            ),
           );
         },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
+        child: Icon(Icons.add),
       ),
     );
   }
